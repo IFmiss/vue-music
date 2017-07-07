701b62b3d20b1b563394e13dcd94bdbe3b747fe2
daiwei <185098535@qq.com>
2017-07-07 23:23:38 +0800

    优化点击歌单列表 正在播放的歌曲会从头播放 以及展示图片的更新

--- src/components/songlist/songlist.vue
+++ src/components/songlist/songlist.vue
@@ -1,18 +1,20 @@
 <template>
 	<div>
 		<div class="songlist" v-if="getSongSheet.info" v-for="(item, index) in getSongSheet.info" @click="playIndex(index)">
-			<div class="listIndex">
-				<span v-show="!showVolume || getCurrentIndex !== index" class="index">{{index + 1}}</span>
-				<i v-show="showVolume && getCurrentIndex === index" class="iconIndex icon-volume-medium"></i>
-			</div>
-			<div class="songInfo">
-				<div class="detail">
-					<p class="name">{{item.name}}</p>
-					<p class="singer">{{item.singer}}</p>
+			<div class="warpper">
+				<div class="listIndex">
+					<span v-show="!showVolume || getCurrentIndex !== index" class="index">{{index + 1}}</span>
+					<i v-show="showVolume && getCurrentIndex === index" class="iconIndex icon-volume-medium"></i>
 				</div>
-				<i class="rightSetting icon-list-circle" @click.stop="showmenu(item.name)"></i>
-				<div class="border-1px"></div>
+				<div class="songInfo">
+					<div class="detail">
+						<p class="name">{{item.name}}</p>
+						<p class="singer">{{item.singer}}</p>
+					</div>
+					<div class="border-1px"></div>
+				</div>
 			</div>
+			<i class="rightSetting icon-list-circle" @click.stop="showmenu(item.name)"></i>
 		</div>
 	</div>
 </template>
@@ -21,6 +23,9 @@
 	export default {
 		methods: {
 			playIndex (index) {
+				if (index === this.$store.getters.getCurrentIndex && this.$store.getters.getMusicSheetList.type === this.$store.getters.getMusiSheetType) {
+					return
+				}
 				store.commit({
 					type: 'setMusiSheetType',
 					sheettype: this.$store.getters.getMusicSheetList.type
@@ -76,69 +81,76 @@
 	@import "../../common/stylus/global.styl"
 	@import "../../common/stylus/border-1px"
 	.songlist
-		display:flex
 		height:54px
 		position:relative
-		&:active
-			background:$list_active
-		.listIndex
+		.warpper
+			display:flex
+			height:54px
+			position:absolute
+			top:0
+			left:0
+			right:0
+			botttom:0
+			&:active
+				background:$list_active
+			.listIndex
+				width:54px
+				height:54px
+				flex: 0 0 54px
+				font-size:0
+				.iconIndex
+					display:inline-block
+					font-size:14px
+					color:$primarycolor
+					width:54px
+					height:54px
+					line-height:54px
+					text-align:center
+				.index
+					display:inline-block
+					font-size:16px
+					font-weight:500
+					color:#BABABA
+					width:54px
+					height:54px
+					line-height:54px
+					text-align:center
+			.songInfo
+				flex:1 1 auto
+				height:54px
+				max-width: calc(100% - 54px)
+				box-sizing:border-box
+				.border-1px
+					border-1px($border_1px)
+				.detail
+					width:auto
+					max-width: calc(100% - 54px)
+					height:100%
+					display:flex
+					padding:5px
+					box-sizing:border-box
+					flex-direction: column
+					justify-content: space-around
+					p
+						margin: 0
+						&.name
+							font-size:14px
+							color:#333
+							font-weight:500
+						&.singer
+							font-size:10px
+							color:#aaa
+							font-weight:300
+		.rightSetting
+			position:absolute
+			right:0
+			top:0
 			width:54px
 			height:54px
-			flex: 0 0 54px
-			font-size:0
-			.iconIndex
-				display:inline-block
-				font-size:14px
-				color:$primarycolor
-				width:54px
-				height:54px
-				line-height:54px
-				text-align:center
-			.index
-				display:inline-block
-				font-size:16px
-				font-weight:500
-				color:#BABABA
-				width:54px
-				height:54px
-				line-height:54px
-				text-align:center
-		.songInfo
-			flex:1 1 auto
-			height:54px
-			max-width: calc(100% - 54px)
-			box-sizing:border-box
-			.border-1px
-				border-1px($border_1px)
-			.detail
-				width:auto
-				max-width: calc(100% - 54px)
-				height:100%
-				display:flex
-				padding:5px
-				box-sizing:border-box
-				flex-direction: column
-				justify-content: space-around
-				p
-					margin: 0
-					&.name
-						font-size:14px
-						color:#333
-						font-weight:500
-					&.singer
-						font-size:10px
-						color:#aaa
-						font-weight:300
-			.rightSetting
-				position:absolute
-				right:0
-				top:0
-				width:54px
-				height:54px
-				line-height:54px
-				font-size:14px
-				text-align:center
-				color:#aaa
-				&:active
-					background:$list_active
+			line-height:54px
+			font-size:14px
+			text-align:center
+			color:#aaa
+			&:active
+				background:$list_active
 </style>
