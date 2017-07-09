717321ab2454489be00d76642365422c24b339a5
daiwei <185098535@qq.com>
2017-07-09 14:09:34 +0800

    优化歌单详情页底部的播放器区域遮挡最后一首歌的列表信息

--- src/components/songlist/songlist.vue
+++ src/components/songlist/songlist.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div class="songlists">
 		<div class="songlist" v-if="getSongSheet.info" v-for="(item, index) in getSongSheet.info" @click="playIndex(index)">
 			<div class="warpper">
 				<div class="listIndex">
@@ -80,77 +80,79 @@
 <style lang="stylus" rel="stylesheet/stylus">
 	@import "../../common/stylus/global.styl"
 	@import "../../common/stylus/border-1px"
-	.songlist
-		height:54px
-		position:relative
-		.warpper
-			display:flex
+	.songlists
+		padding-bottom:46px
+		.songlist
 			height:54px
-			position:absolute
-			top:0
-			left:0
-			right:0
-			botttom:0
-			&:active
-				background:$list_active
-			.listIndex
+			position:relative
+			.warpper
+				display:flex
+				height:54px
+				position:absolute
+				top:0
+				left:0
+				right:0
+				botttom:0
+				&:active
+					background:$list_active
+				.listIndex
+					width:54px
+					height:54px
+					flex: 0 0 54px
+					font-size:0
+					.iconIndex
+						display:inline-block
+						font-size:14px
+						color:$primarycolor
+						width:54px
+						height:54px
+						line-height:54px
+						text-align:center
+					.index
+						display:inline-block
+						font-size:16px
+						font-weight:500
+						color:#BABABA
+						width:54px
+						height:54px
+						line-height:54px
+						text-align:center
+				.songInfo
+					flex:1 1 auto
+					height:54px
+					max-width: calc(100% - 54px)
+					box-sizing:border-box
+					.border-1px
+						border-1px($border_1px)
+					.detail
+						width:auto
+						max-width: calc(100% - 54px)
+						height:100%
+						display:flex
+						padding:5px
+						box-sizing:border-box
+						flex-direction: column
+						justify-content: space-around
+						p
+							margin: 0
+							&.name
+								font-size:14px
+								color:#333
+								font-weight:500
+							&.singer
+								font-size:10px
+								color:#aaa
+								font-weight:300
+			.rightSetting
+				position:absolute
+				right:0
+				top:0
 				width:54px
 				height:54px
-				flex: 0 0 54px
-				font-size:0
-				.iconIndex
-					display:inline-block
-					font-size:14px
-					color:$primarycolor
-					width:54px
-					height:54px
-					line-height:54px
-					text-align:center
-				.index
-					display:inline-block
-					font-size:16px
-					font-weight:500
-					color:#BABABA
-					width:54px
-					height:54px
-					line-height:54px
-					text-align:center
-			.songInfo
-				flex:1 1 auto
-				height:54px
-				max-width: calc(100% - 54px)
-				box-sizing:border-box
-				.border-1px
-					border-1px($border_1px)
-				.detail
-					width:auto
-					max-width: calc(100% - 54px)
-					height:100%
-					display:flex
-					padding:5px
-					box-sizing:border-box
-					flex-direction: column
-					justify-content: space-around
-					p
-						margin: 0
-						&.name
-							font-size:14px
-							color:#333
-							font-weight:500
-						&.singer
-							font-size:10px
-							color:#aaa
-							font-weight:300
-		.rightSetting
-			position:absolute
-			right:0
-			top:0
-			width:54px
-			height:54px
-			line-height:54px
-			font-size:14px
-			text-align:center
-			color:#aaa
-			&:active
-				background:$list_active
+				line-height:54px
+				font-size:14px
+				text-align:center
+				color:#aaa
+				&:active
+					background:$list_active
 </style>
