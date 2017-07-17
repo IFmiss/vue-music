53340fb7355cd529773817f7399b732a990677de
ifmiss <185098535@qq.com>
2017-07-17 16:41:32 +0800

    find 点击幻灯片播放音乐   添加三个类型的显示样式

--- src/components/findrecommend/findrecommend.vue
+++ src/components/findrecommend/findrecommend.vue
@@ -1,19 +1,35 @@
 <template>
 	<div class="findrecommend">
-		<swiper :options="swiperOption"  ref="mySwiper" v-if="reconmmend">  
+		<swiper :options="swiperOption"  ref="mySwiper" v-if="reconmmend.findmusic">  
             <!-- 这部分放你要渲染的那些内容 -->  
-            <swiper-slide v-for="item in reconmmend.reconmmend.recommendImageList">
+            <swiper-slide v-for="item in reconmmend.findmusic.reconmmend.recommendImageList" :key="item.id" :songlist="JSON.stringify(item.songList)">
             	<img class="image" :src="item.src" alt="">
             	<span class="type" :style="{backgroundColor:item.color}">{{item.type}}</span>
             </swiper-slide>  
             <!-- 这是轮播的小圆点 -->  
             <div class="swiper-pagination" slot="pagination"></div>  
-        </swiper>  
+        </swiper>
+        <div class="recommend-type">
+        	<div class="div-type">
+        		<i class="icon-type icon-fm"></i>
+        		<p class="title">私人FM</p>
+        	</div>
+        	<div class="div-type">
+        		<i class="icon-type icon-date"></i>
+        		<p class="title">每日听歌推荐</p>
+        	</div>
+        	<div class="div-type">
+        		<i class="icon-type icon-rank-list"></i>
+        		<p class="title">云音乐热歌榜</p>
+        	</div>
+        </div>
 	</div>
 </template>
 <script>
 	import store from '../../store'
 	import { swiper, swiperSlide } from 'vue-awesome-swiper'
+	// import { mapState } from 'vuex'
+	import { mapGetters } from 'vuex'
 	export default {
 		data () {
 	      return {
@@ -35,17 +51,38 @@
 					// 这个位置放swiper的回调方法
 					this.page = swiper.realIndex + 1
 					this.index = swiper.realIndex
+				},
+				onClick: swiper => {
+					const index = swiper.activeIndex
+					// alert(swiper.slides[index].getAttribute('songlist'))
+					store.commit({
+						type: 'setMusicList',
+						list: JSON.parse(swiper.slides[index].getAttribute('songlist'))
+					})
+					store.commit({
+						type: 'playIndex',
+						index: 0
+					})
+					store.dispatch({
+						type: 'set_MusicDetail',
+						isShow: true
+					})
 				}
 			}
 	      }
 	    },
 	    computed: {
-			reconmmend () {
-				return store.getters.getAllInfo.findmusic
-			},
+			// reconmmend () {
+			// 	return store.getters.getAllInfo.findmusic
+			// },
+			...mapGetters({
+				reconmmend: 'getAllInfo'
+			}),
 			swiper () {
 				return this.$refs.mySwiper.swiper
 			}
+	    },
+	    methods: {
 	    },
 		mounted () {
 			// you can use current swiper instance object to do something(swiper methods)
@@ -95,5 +132,40 @@
 					margin:0 4px
 					&.swiper-pagination-bullet-active
 						background:$primarycolor!important
+		.recommend-type
+			width:100%
+			height:90px
+			display:flex
+			align-items:center
+			.div-type
+				flex:1
+				height:100%
+				width:33.333333%
+				font-size:0
+				display:flex
+				flex-direction: column
+				justify-content: center
+				align-items: center
+				padding:0 10px
+				.icon-type
+					display:inline-block
+					color:$primarycolor
+					font-size:32px
+					width:50px
+					height:50px
+					line-height:50px
+					text-align:center
+					border:1px solid $primarycolor
+					border-radius:50%
+				.title
+					font-size:12px
+					font-weight:400
+					margin:0
+					margin-top:8px
+					text-overflow:ellipsis
+					overflow:hidden
+					white-space:nowrap
+					width:100%
+					text-align:center
 				
 </style>
