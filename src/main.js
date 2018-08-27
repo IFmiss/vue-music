4d336da7a79234758d9b9cd459e7bc5c62f0251c
ifmiss <185098535@qq.com>
2018-08-28 00:28:18 +0800

    添加移动适配

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import './utils/rem.js'
+import 'amfe-flexible'
+
 Vue.config.productionTip = false
 
 new Vue({
