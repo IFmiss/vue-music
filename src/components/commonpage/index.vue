188f3190241b5eb81f8980e4c56eeadaf7c42be9
ifmiss <185098535@qq.com>
2018-08-30 23:26:06 +0800

    添加通用页 commonpage 编写登陆页

---
+++ src/components/commonpage/index.vue
@@ -0,0 +1,63 @@
+<template lang="pug">
+  .common-page
+    .auto-header
+      .left
+        i.icon-back
+      .title 你好
+      .right
+        slot(name="header")
+    .auto-body
+      slot(name="content")
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss" scoped>
+// 通用页面内容
+.common-page{
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  // 头部
+  .auto-header{
+    flex: 0 0 $auto_h;
+    width: 100%;
+    color: #fff;
+    font-size: $f_auto_l;
+    background: $primary_color;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 $auto_padding_l_r;
+    box-sizing: border-box;
+    .left,.right{
+      flex: p2r(1.8rem);
+      display: flex;
+      align-items: center;
+      i, span{
+        flex: 0 0 p2r(0.7rem);
+        margin: 0 p2r(0.2rem);
+      }
+    }
+    .left{
+      justify-content: flex-start;
+    }
+    .right{
+      justify-content: flex-end;
+    }
+    .title{
+      flex: 1 1 auto;
+      @include els();
+    }
+  }
+  // 内容
+  .auto-body{
+    flex: 1 1 auto;
+    padding: $auto_padding_t_b $auto_padding_l_r;
+    background: #fff;
+  }
+}
+</style>
--- src/views/login/sublogin/index.vue
+++ src/views/login/sublogin/index.vue
@@ -1,16 +1,32 @@
 <template lang="pug">
   .sub-login.auto-content
-    .auto-header
-      span 你好
-    .auto-body aaaa
+    Commonpage
+      //- .header-slot(slot="header") 1
+      .content-slot(slot="content")
+        .text-input
+          i.icon-phone
+          input(type="text")
+        .text-input
+          i.icon-pwd
+          input(type="password")
+        input(type="button")
 </template>
 <script>
+import Commonpage from '@/components/commonpage'
 export default {
+  components: {
+    Commonpage
+  },
   created () {
   }
 }
 </script>
 <style lang="scss" scoped>
-.auto-content{
+.content-slot{
+  width: p2r(5rem);
+  margin-top: p2r(1rem);
+  margin: 0 auto;
+  .text-input{
+  }
 }
 </style>
