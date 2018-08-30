aefe87450a597d54ff46feff3664a9a4f985886e
DavidDai <185098535@qq.com>
2018-08-30 10:59:45 +0800

    修改build生成的index文件名称

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/vue-music.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
